52214746f989d0014bce69dd1c8d16bd3343edaf
Jayakumar Rajendran <jaiaheedhan@gmail.com>
2016-12-19 16:30:42 +0530

    adding class and method level comments

--- WebCrawler7/src/main/java/com/rest/crawler/controller/CrawlerController.java
+++ WebCrawler7/src/main/java/com/rest/crawler/controller/CrawlerController.java
@@ -2,18 +2,24 @@ package com.rest.crawler.controller;
 
 import javax.ws.rs.core.Response;
 
-
+/* this is the class to handle all type of request from the index page
+ * this class is acting as a controller Using Jersey, restful webservices 
+ * 
+ * */
 public class CrawlerController {
-	
-	Response CrowlerText(){
+	/*
+	 * this method will produce text based output*/
+	public Response CrowlerText(){
 		return null;
 	}
-	
-	Response CrowlerXML(){
+	/*
+	 * this method will produce xml based output*/
+	public Response CrowlerXML(){
 		return  null;
 	}
-	
-	Response CrowlerJSON(){
+	/*
+	 * this method will produce json based output*/
+	public Response CrowlerJSON(){
 		return  null;
 	}
 }
